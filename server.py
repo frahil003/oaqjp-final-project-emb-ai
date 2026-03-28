529718da28159ddff61916016f2b543ac2ca9606
Frank Hilgenberg <frank.hilgenberg@franks-divecenter.de>
2026-03-28 19:02:32 +0100

    Add more tasks

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_detector():
+
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    emotions = emotion_detector(text_to_analyze)
+
+    return (
+        f"The given given statement, the system response is "
+        f"'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, "
+        f"'fear': {emotions['fear']}, 'joy': {emotions['joy']} and "
+        f"'sadness': {emotions['sadness']}. The dominant emotion is {emotions['dominant_emotion']}."
+    )
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
